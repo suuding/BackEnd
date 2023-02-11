479b3627af30dc017ab7708c28ddf4f00e7412b5
zigimi <chju0905@pusan.ac.kr>
2023-02-11 20:44:27 +0900

    설정페이지 수정시 db 수정 기능 추가

--- src/main/java/com/bstarbackend/bstar/web/SettingController.java
+++ src/main/java/com/bstarbackend/bstar/web/SettingController.java
@@ -1,15 +1,13 @@
 package com.bstarbackend.bstar.web;
 
 import com.bstarbackend.bstar.service.SettingsService;
+import com.bstarbackend.bstar.web.dto.SettingUpdateRequestDto;
 import com.bstarbackend.bstar.web.dto.SettingsResponseDto;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -18,9 +16,15 @@ public class SettingController {
 
     private final SettingsService settingsService;
     @GetMapping("/setting/info")
-    public SettingsResponseDto test(Authentication authentication, @AuthenticationPrincipal UserDetails userDetails) {
+    public SettingsResponseDto enroll(Authentication authentication, @AuthenticationPrincipal UserDetails userDetails) {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         SettingsResponseDto settings = settingsService.findByEmail(oAuth2User.getAttribute("name"), oAuth2User.getAttribute("email"));
         return settings;
+    }
+
+    @PutMapping("/setting/info")
+    public void update(Authentication authentication, @RequestBody SettingUpdateRequestDto requestDto) {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        settingsService.update(oAuth2User.getAttribute("email"), requestDto);
     }
 }
